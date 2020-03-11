e72a204247bebd2a4af7c3e3e691b348cafd91b6
Tom Jennings <thomas.jennings@ibm.com>
2020-03-11 21:02:15 +0000

    added to return failed if it failed as well as adding a common stack trace error for car not found with car not found

--- blockchain-restful-service/finish/src/main/java/org/example/QueryCar.java
+++ blockchain-restful-service/finish/src/main/java/org/example/QueryCar.java
@@ -1,15 +1,14 @@
 package org.example;
 
 import java.nio.file.Paths;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.QueryParam;
-
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.hyperledger.fabric.gateway.Contract;
 import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.Network;
@@ -30,17 +29,21 @@ public class QueryCar {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String Querycar(@QueryParam("Key")String Key) 
 	{
+	
 		byte[] result = null;
 		String outputString = "";
+		String passedOutput = "";
+		String failedOutput = "";
+		String noCar = "";
 		try {
 		Path walletPath = Paths.get("wallet");
 		Wallet wallet = Wallet.createFileSystemWallet(walletPath);
 		
 		// load a CCP
-		Path networkConfigPath = Paths.get("first-network", "connection-org1.yaml");
+		Path networkConfigPath = Paths.get("1-Org-Local-Fabric-Org1_connection.json");
 		
 		Gateway.Builder builder = Gateway.createBuilder();
-		builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
+		builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
 		
 		try (Gateway gateway = builder.connect()) {
 
@@ -51,14 +54,26 @@ public class QueryCar {
 			result = contract.evaluateTransaction("queryCar", Key);
 			System.out.println(new String(result));
 			outputString = new String(result);
-
+			passedOutput = "Queried car Successfully. \nKey = " + Key + "\nDetails = " + outputString;
+			
+			
 			}
 		}
 		catch (Exception e){
 			e.printStackTrace();
+			failedOutput = "Failed \n";
+			
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			String sStackTrace = sw.toString();
+			if (sStackTrace.contains("org.hyperledger.fabric.gateway.ContractException: transaction returned with failure: Error:")) {
+				noCar = "Car not found";
+			}
+			
 		}
 		
-		return "Queried car Successfully. \nKey = " + Key + "\nDetails = " + outputString;
+		return passedOutput + failedOutput + noCar;
 	}
 
 	
@@ -69,16 +84,18 @@ public class QueryCar {
 		
 		byte[] result = null;
 		String outputString = "";
-		
+		String passedOutput = "";
+		String failedOutput = "";
 		
 		try {
 			Path walletPath = Paths.get("wallet");
 			Wallet wallet = Wallet.createFileSystemWallet(walletPath);
 			// load a CCP
-			Path networkConfigPath = Paths.get("first-network", "connection-org1.yaml");
+		
+			Path networkConfigPath = Paths.get("1-Org-Local-Fabric-Org1_connection.json");
 						
 			Gateway.Builder builder = Gateway.createBuilder();
-			builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
+			builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
 			
 			try (Gateway gateway = builder.connect()) {
 
@@ -86,12 +103,10 @@ public class QueryCar {
 				Network network = gateway.getNetwork("mychannel");
 				Contract contract = network.getContract("fabcar");
 
-				
-
 				result = contract.evaluateTransaction("queryAllCars");
 				System.out.println(new String(result));
 				outputString = new String(result);
-				
+				passedOutput = "Queried all Cars Successfully. \nCars are:\n " + outputString;
 			}
 				catch(Exception ex) 
 				{
@@ -101,9 +116,10 @@ public class QueryCar {
 			catch (Exception e)
 			{
 				e.printStackTrace();
+				failedOutput = "Failed to retrieve all cars"; 
 			}
 			
-			return "Queried all Cars Successfully. \nCars are:\n " + outputString;
+			return passedOutput + failedOutput;
 		
 	}
 }
