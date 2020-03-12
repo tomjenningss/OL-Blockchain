9835946e86571a8ce732e941176bfce13dc71f7f
david jennings <david.jennings@dwp.gov.uk>
2020-03-12 08:56:21 +0000

    Added json car response - put

--- blockchain-restful-service/OL-Blockchain/src/main/java/io/openliberty/blockchain/communication/AddCar.java
+++ blockchain-restful-service/OL-Blockchain/src/main/java/io/openliberty/blockchain/communication/AddCar.java
@@ -15,7 +15,10 @@ import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.Network;
 import org.hyperledger.fabric.gateway.Wallet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
+import io.openliberty.blockchain.model.*;
 
 @javax.ws.rs.Path("AddCar")
 
@@ -35,46 +38,62 @@ public class AddCar {
 			@QueryParam("owner") String owner) 
 	{
 		
-
+		String retString = "";
+		
+		ObjectMapper mapper = new ObjectMapper();
+	    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+	    
 		try {
 			
-		Path walletPath = Paths.get("wallet");
-		Wallet wallet = Wallet.createFileSystemWallet(walletPath);
-		
-		// load a CCP
-		Path networkConfigPath = Paths.get("1-Org-Local-Fabric-Org1_connection.json");
-		
-		Gateway.Builder builder = Gateway.createBuilder();
-		builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
-		
-		try (Gateway gateway = builder.connect()) {
-
-			// get the network and contract
-			Network network = gateway.getNetwork("mychannel");
-			Contract contract = network.getContract("fabcar");
+			Path walletPath = Paths.get("wallet");
+			Wallet wallet = Wallet.createFileSystemWallet(walletPath);
 			
-			//String key = make + owner;
+			// load a CCP
+			Path networkConfigPath = Paths.get("1-Org-Local-Fabric-Org1_connection.json");
 			
-			//System.out.print("Make is " + make);
-			//System.out.print("Model is " + model);
-			//System.out.print("Colour is " + colour);
-			//System.out.print("Owner is " + owner);
+			Gateway.Builder builder = Gateway.createBuilder();
+			builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
 			
-			//System.out.println("Key = " + Key);
-			
-			if (make == null) {
-				make = "unknown";
+			try (Gateway gateway = builder.connect()) {
+	
+				// get the network and contract
+				Network network = gateway.getNetwork("mychannel");
+				Contract contract = network.getContract("fabcar");
+				
+				String Key = make + owner;
+				
+				//initialise a new car from the class car and populate the properties
+				//this is used to return the json car object
+				car newCar = new car();
+				
+				newCar.setMake(make);
+				newCar.setModel(model);
+				newCar.setColour(colour);
+				newCar.setOwner(owner);
+				newCar.setKey(Key);
+				
+				if (make == null) {
+					make = "unknown";
+				}
+				
+				//use the car object to add to the ledger
+				contract.submitTransaction("createCar", newCar.getKey(), newCar.getMake(), newCar.getModel(), newCar.getColour(), newCar.getOwner());
+			    
+				//convert the newCar object to a json string
+				retString = mapper.writeValueAsString(newCar);
+				}
 			}
-			contract.submitTransaction(make, model, colour, owner);
+			catch (Exception e){
+				error newError = new error();
+				
+				newError.setCode("50001");
+				newError.setMessage(e.getMessage());
+				
+				retString = mapper.writeValueAsString(newError);
+				
+				e.printStackTrace();
 			}
-		}
-		catch (Exception e){
-			e.printStackTrace();
-		}
 		
-		String parameters = "\nOwner = " + owner + "\nMake = " + make + "\nModel = " + "\nColour = " + colour;
-		String key = "\nTo Query recently added contents the Key is: \n" + make + owner + "\n";
-		
-		return parameters + key;
+		return retString;
 	}	
 }
